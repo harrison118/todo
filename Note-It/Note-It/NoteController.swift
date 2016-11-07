435bbefe5e2033e83e7e57ce2881d6ca1ccc87aa
harrison118 <harrison.stevens10@hotmail.co.uk>
2016-11-07 15:44:56 +0000

    Hide and show keyboard, with the ability to change name of notes, tho will not save, being able to move back and fourth between details view and note view

--- Note-It/Note-It/NoteController.swift
+++ Note-It/Note-It/NoteController.swift
@@ -8,12 +8,53 @@
 
 import UIKit
 
-class NoteController: UIViewController {
+class NoteController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var noteField: UITextView!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
+    @IBAction func dismissKeyboard(_ sender : UIBarButtonItem ){
+        self.textField.resignFirstResponder()
+        self.noteField.resignFirstResponder()
+    }
+    
+    public var noteID : Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.textField.delegate = self
+        self.noteField.delegate = self
+        if let id:Int = self.noteID {
+            print("View did load \(id)")
+            if let note:Note = try? Notes.sharedInstance.getNote(atIndex: id) {
+            self.title = note.title
+            self.textField.text = note.title
+            self.noteField.text = note.text
+            }
 
         // Do any additional setup after loading the view.
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("Finished Editing the title")
+        
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print("Finished Editing the Note")
+    }
+    
+    func keyboardWillshow(_ notification : NSNotification){
+        print("Keyboard will Show")
+        self.doneButton.tintColor = nil
+    }
+    
+    func KeyboardWillHide(_ notification = NSNotification) {
+        print("keyboard will Hide ")
+        self.doneButton.tintColor = UIcolor.clear
+
     }
 
     override func didReceiveMemoryWarning() {
